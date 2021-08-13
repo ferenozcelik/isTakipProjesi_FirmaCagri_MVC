51c12d72c22409976bbaaddfd35e346f24aadf0d
Fatih Eren ÖZÇELİK <eren.ozcelik@std.antalya.edu.tr>
2021-08-13 19:49:37 +0300

    Add project files.

---
+++ MVCFirmaCagri/Controllers/DefaultController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCFirmaCagri.Models.Entity;
+
+namespace MVCFirmaCagri.Controllers
+{
+    public class DefaultController : Controller
+    {
+
+        DbisTakipEntities db = new DbisTakipEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AktifCagrilar()
+        {
+            var aktifCagrilar = db.TblCagrilar.Where(x => x.Durum == true && x.CagriFirma == 4).ToList();
+            return View(aktifCagrilar);
+        }
+
+        public ActionResult PasifCagrilar()
+        {
+            var pasifCagrilar = db.TblCagrilar.Where(x => x.Durum == false && x.CagriFirma == 4).ToList();
+            return View(pasifCagrilar);
+        }
+
+        [HttpGet]
+        public ActionResult YeniCagri()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniCagri(TblCagrilar p)
+        {
+            p.Durum = true;
+            p.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.CagriFirma = 4;
+            db.TblCagrilar.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("AktifCagrilar");
+        }
+
+
+        public ActionResult CagriDetay(int id)
+        {
+            var cagri = db.TblCagriDetay.Where(x => x.Cagri == id).ToList();
+            return View(cagri);
+        }
+        
+    }
+}
